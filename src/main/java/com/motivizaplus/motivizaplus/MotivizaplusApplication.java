41a1004b54179e2f398b2b15c50e8028d04619e5
ShiroVs <robert.vs@outlook.com>
2025-06-06 11:33:40 -0500

    Repositorio creado con SB

---
+++ src/main/java/com/motivizaplus/motivizaplus/MotivizaplusApplication.java
@@ -0,0 +1,13 @@
+package com.motivizaplus.motivizaplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivizaplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotivizaplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motivizaplus/motivizaplus/MotivizaplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motivizaplus.motivizaplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivizaplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
