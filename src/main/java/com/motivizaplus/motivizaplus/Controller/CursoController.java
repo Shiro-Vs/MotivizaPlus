b783339839d55a932a0f1b12b9b935e258acec24
JRiveraAlmanza <jhonyrivera.1821@gmail.com>
2025-06-07 12:07:20 -0500

    Se agregó la vista Cursos

--- src/main/java/com/motivizaplus/motivizaplus/Controller/CursoController.java
+++ src/main/java/com/motivizaplus/motivizaplus/Controller/CursoController.java
@@ -1,8 +1,15 @@
 package com.motivizaplus.motivizaplus.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class CursoController {
+    
+    @GetMapping("/cursos")
+    public String mostrarCursos (){
+        return "cursos";
+    }
     
 }
